0741858334f36a9aba585cf3acdc1cab6f4f1558
vdmeer <sven@vandermeer.de>
2015-06-19 12:15:06 +0100

    maintenance for v004

--- src/main/java/de/vandermeer/execs/Skb_ExecCli.java
+++ src/main/java/de/vandermeer/execs/Skb_ExecCli.java
@@ -25,7 +25,7 @@ import org.apache.commons.cli.PosixParser;
  * Command line parser for the service executor.
  * 
  * @author     Sven van der Meer &lt;vdmeer.sven@mykolab.com&gt;
- * @version    v0.0.4-SNAPSHOT build 150618 (18-Jun-15) for Java 1.8
+ * @version    v0.0.4 build 150619 (19-Jun-15) for Java 1.8
  */
 public class Skb_ExecCli {
 
